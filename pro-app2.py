c25f112fc01f5c7f643c284e037ec9cb0481b300
Asha Sandeep <162896750+aashasandeep@users.noreply.github.com>
2024-12-07 00:38:45 +0530

    Add files via upload

---
+++ pro-app2.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+#title
+
+st.title("Fake News Classification App")
+
+#Header/Subheader
+
+st.header("Select a News Title")
+
+#text
+
+st.text("TWEETS DISASTER")
+
+sentence = ""
+# add a selection box to retrieve the titles and their content
+title = st.selectbox("Choose a news title",['title'])
+
+
+
+
+
+
+
+
+ 
